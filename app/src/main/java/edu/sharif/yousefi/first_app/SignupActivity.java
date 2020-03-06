7d7a9b1a2c81bcf12cfa177756ddc42b0dedcf08
zahrayousefijamarani <z.y.j.1379@gmail.com>
2020-03-06 20:06:11 +0330

    add classes

--- app/src/main/java/edu/sharif/yousefi/first_app/SignupActivity.java
+++ app/src/main/java/edu/sharif/yousefi/first_app/SignupActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,7 +13,6 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class SignupActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
