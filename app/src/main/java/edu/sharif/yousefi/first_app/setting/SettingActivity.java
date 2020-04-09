c50ed3dfde2b87e1f1a8ee05a11bf05e1bebb8cb
zahrayousefijamarani <z.y.j.1379@gmail.com>
2020-04-10 03:50:35 +0430

    add changing

--- app/src/main/java/edu/sharif/yousefi/first_app/setting/SettingActivity.java
+++ app/src/main/java/edu/sharif/yousefi/first_app/setting/SettingActivity.java
@@ -1,14 +1,44 @@
 package edu.sharif.yousefi.first_app.setting;
 
+import android.content.SharedPreferences;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.os.Bundle;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import edu.sharif.yousefi.first_app.R;
-public class SettingActivity extends PreferenceActivity {
+
+public class SettingActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preference_setting);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals("low_data_key")) {
+            CheckBoxPreference preference = (CheckBoxPreference) findPreference(key);
+
+            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+            Date date = new Date();
+            preference.setSummary("You modified this at " + formatter.format(date));
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
     }
 }
